49c767874d86a292e2ef966eab08ce67ac916e42
Wesley Darton <wdarton@lendingclub.com>
2023-05-04 11:48:03 -0600

    custom checkbox now works with horizontal forms

--- templates/element/Form/Components/custom_checkbox.php
+++ templates/element/Form/Components/custom_checkbox.php
@@ -1,4 +1,14 @@
 <?php
+
+// Usage example
+// echo $this->element('Avalon.Form/Components/custom_checkbox', [
+// 	'name' => 'recipient_residential',
+// 	'label' => 'Residential Address',
+// 	'state' => '',
+// 	'formType' => 'horiz',
+// ]);
+
+
 $idName = str_replace('_', '-', $name);
 if (!isset($label)) {
 	$label = ucwords(str_replace('_', ' ', $name));
@@ -16,9 +26,23 @@ if (!isset($customClass)) {
 }
 ?>
 
+<?php if ($formType == strtolower('horiz')): ?>
+<div class="row mb-3">
+	<label class="form-check-label col-sm-4" for="<?= $idName ?>">
+		<?= $label ?>
+	</label>
+	<div class="col-sm-8">
+		<div class="form-check">
+			<input class="form-check-input  <?= $customClass ?>" type="checkbox" id="<?= $idName ?>" name="<?= $name ?>" value="1" <?= $checked ?>>
+		</div>
+	</div>
+</div>
+<?php else: ?>
 <div class="form-check">
 	<input class="form-check-input  <?= $customClass ?>" type="checkbox" id="<?= $idName ?>" name="<?= $name ?>" value="1" <?= $checked ?>>
 	<label class="form-check-label" for="<?= $idName ?>">
 		<?= $label ?>
 	</label>
 </div>
+
+<?php endif; ?>
